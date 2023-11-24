15ce2f5a0aab1a48f12b598d1b6e0a0f785028be
Björn Paulsen <bjorn.paulsen@medexa.se>
2023-11-24 10:48:33 +0100

    refactor names and comments

---
+++ WpfTestExtendedTabControl/SettingsAwareTabControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfTestExtendedTabControl
+{
+    public class SettingsAwareTabControl : TabControl
+    {
+        // Register the dependency property for the settings bar.
+        public static readonly DependencyProperty SettingsControlProperty = 
+            DependencyProperty.Register(
+            nameof(SettingsControl),
+            typeof(UIElement),
+            typeof(SettingsAwareTabControl),
+            new FrameworkPropertyMetadata(null, 
+                FrameworkPropertyMetadataOptions.AffectsMeasure | 
+                FrameworkPropertyMetadataOptions.AffectsArrange)
+        );
+
+        // Property wrapper
+        public UIElement SettingsControl
+        {
+            get => (UIElement)GetValue(SettingsControlProperty);
+            set => SetValue(SettingsControlProperty, value);
+        }
+    }
+}
